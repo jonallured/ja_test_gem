13242343946cf6557ee3016886799e8c33088098
Jon Allured <jon@jonallured.com>
2024-08-18 08:49:39 -0500

    Initial commit

---
+++ lib/ja_test_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ja_test_gem/version"
+
+module JaTestGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ja_test_gem_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JaTestGem do
+  it "has a version number" do
+    expect(JaTestGem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
